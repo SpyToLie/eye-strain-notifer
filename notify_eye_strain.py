247a503fd753733f07028b81a81d24787f41a991
BrassJeans <BrassJeans@users.noreply.github.com>
2020-05-25 20:32:55 +0200

    Update notify_eye_strain.py

--- notify_eye_strain.py
+++ notify_eye_strain.py
@@ -4,7 +4,10 @@ import time
 
 if __name__ == '__main__':
 	while True:
-		time.sleep(1200)
-		notification = ToastNotifier()
-		notification.show_toast("Reminder to look away for 20 seconds!",
-		                        duration=3, msg=" ")
+		try:
+			time.sleep(1200)
+			notification = ToastNotifier()
+			notification.show_toast("Reminder to look away for 20 seconds!",
+			                        duration=3, msg=" ")
+		except:
+			pass
