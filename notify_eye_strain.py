c5ff07d3b9fabdf62bd99bdc7c6aa21ea62dfec0
BrassJeans <BrassJeans@users.noreply.github.com>
2020-05-25 20:29:35 +0200

    Update notify_eye_strain.py

--- notify_eye_strain.py
+++ notify_eye_strain.py
@@ -1,23 +1,10 @@
 from win10toast import ToastNotifier
-from idle_time import IdleMonitor
 import time
 
 
-def idle_time():
-	monitor = IdleMonitor.get_monitor()
-	idle_time = monitor.get_idle_time()
-	return idle_time
-
-
-def notify():
-	notification = ToastNotifier()
-	notification.show_toast("Reminder to look away for 20 seconds!",
-	                        duration=3, msg=" ")
-
-
 if __name__ == '__main__':
-	proceed = True
-	while proceed:
+	while True:
 		time.sleep(1200)
-		if idle_time() < 600:
-			notify()
+		notification = ToastNotifier()
+		notification.show_toast("Reminder to look away for 20 seconds!",
+		                        duration=3, msg=" ")
