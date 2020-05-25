101d417dbe4a2ec723d75b108e80b9396ca7ff7f
BrassJeans <BrassJeans@users.noreply.github.com>
2020-05-25 18:39:41 +0200

    changed py file name

--- notify_eye_strain.py
+++ notify_eye_strain.py
@@ -3,7 +3,7 @@ from idle_time import IdleMonitor
 import time
 
 
-def monitor_idle_time():
+def idle_time():
 	monitor = IdleMonitor.get_monitor()
 	idle_time = monitor.get_idle_time()
 	return idle_time
@@ -11,13 +11,13 @@ def monitor_idle_time():
 
 def notify():
 	notification = ToastNotifier()
-	notification.show_toast("Reminder to look away from the screen for 20 seconds!",
-	                        duration=3, msg="If possible, look at something that is at least 20 meters away.")
+	notification.show_toast("Reminder to look away for 20 seconds!",
+	                        duration=3, msg=" ")
 
 
 if __name__ == '__main__':
 	proceed = True
 	while proceed:
 		time.sleep(1200)
-		if monitor_idle_time() > 60:
+		if idle_time() < 600:
 			notify()
